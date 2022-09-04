0877267d3a8322144f1c3100ef1516db97ff8e37
Kirill K <kovalev.kirill.a@gmail.com>
2022-09-04 21:39:46 +0300

    chore: init

---
+++ main.py
@@ -0,0 +1,20 @@
+"""Server to manage project state."""
+
+if __name__ == "__main__":
+    pass
+
+    # TODO Add data storage
+
+    # TODO Add dummy handler for new issues
+    # TODO Add dummy handler for removed issues
+
+    # TODO Manage issues for GitHub
+    # - Create tickets
+    # - Close tickets
+    # - List issues
+
+    # TODO Add registration for project
+    # TODO Add registration for engineer
+
+    # TODO Think on how to accept payments
+    # TODO Think on giving rewards
