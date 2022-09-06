e783dad7e895653bbe9df04ef17184c7d3050383
Kirill K <agrrh@users.noreply.github.com>
2022-09-06 19:38:38 +0300

    feat: backend (#1)
    * feat: some backend parts
    
    * chore: add docker-compose
    
    * fix: echo model
    
    * chore: update todos
    
    * chore: add makefile

---
+++ README.md
@@ -0,0 +1,6 @@
+# iss-control-center
+
+Issue Manager server part.
+
+- Server to manage projects state
+- Heavily inspired by [PDD](https://www.yegor256.com/2017/04/05/pdd-in-action.html) concept
---
+++ backend/iss-control-center/main.py
@@ -0,0 +1,43 @@
+"""Server to manage project state."""
+
+from fastapi import FastAPI
+
+from .models.github import GitHubWebhookPayload
+
+
+app = FastAPI()  # noqa: W001
+
+# TODO Add data storage
+
+
+@app.get("/")
+def read_root():
+    """Index page."""
+
+    return {"Message": "Change me later"}
+
+
+# TODO Add dummy handler for new issues
+# TODO Add dummy handler for removed issues
+
+# TODO Manage issues for GitHub
+# - Create tickets
+# - Close tickets
+# - List issues
+
+# TODO Add registration for project
+# TODO Add registration for engineer
+
+# TODO Think on how to accept payments
+# TODO Think on giving rewards
+
+# TODO Check GitHub secret to evade processing non-trusted requests
+
+
+@app.post("/webhook/github", status_code=201)
+def webhook_github(payload: GitHubWebhookPayload):
+    """Processing GitHub webhook."""
+
+    # TODO Send update event to message bus, so it could clone repo and check for diffs
+
+    return payload.dict()
